4c8cbf4589711143cce540ab5e874768bbf4010c
alu0100622492 <alu0100622492@ull.edu.es>
2016-10-29 01:46:42 +0100

    Solucionando problemas

--- linea-comando-iaas.js
+++ linea-comando-iaas.js
@@ -38,17 +38,12 @@ function initialize(directorio) {
 
 
     //copiamos gulpfile a nuestro directorio
-    // fs.copyFile(path.join(process.cwd(), 'node_modules','gitbook-start-team-noejaco2017','gulpfile.js'), path.join(process.cwd(), directorio , 'gulpfile.js'),function(err){
-    //     if(err)
-    //       console.log(err);
-    //      console.log("Tarea gulp añadida a gulpfile")
-    // });
-
-    fs.copy(path.join(process.cwd(), 'node_modules','gitbook-start-team-noejaco2017','gulpfile.js'),path.join(process.cwd(), directorio , 'gulpfile.js'),function(err){
-      if(err)
-      console.log(err);
-      console.log("Tarea gulp añadida a gulpfile")
+    fs.copyFile(path.join(process.cwd(), 'node_modules','gitbook-start-team-noejaco2017','gulpfile.js'), path.join(process.cwd(), directorio , 'gulpfile.js'),function(err){
+        if(err)
+          console.log(err);
+         console.log("Tarea gulp añadida a gulpfile")
     });
+
 
 
 
